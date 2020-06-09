60f2e1b3ae800c854b879a63f877223d4d78058c
Eduardo Azevedo <53442593+enazevedo@users.noreply.github.com>
2020-06-10 00:42:06 +1000

    Initial version

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/IFTTT')
+def ifttt_home():
+    return 'Circulating. Nothing to see here at the home of IFTTT...'
+
